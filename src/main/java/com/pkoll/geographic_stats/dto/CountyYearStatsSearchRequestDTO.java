16be6bf46a64f90fb9579f32c1cf6cc39a4ef650
Panos <pk5492@outlook.com>
2025-07-10 16:08:03 +0300

    Use primitives for pageNumber, pageSize

--- src/main/java/com/pkoll/geographic_stats/dto/CountyYearStatsSearchRequestDTO.java
+++ src/main/java/com/pkoll/geographic_stats/dto/CountyYearStatsSearchRequestDTO.java
@@ -8,6 +8,6 @@ public record CountyYearStatsSearchRequestDTO(
         @Schema(example = "Southern Europe") String region,
         @Schema(example = "1990") Integer yearFrom,
         @Schema(example = "2000") Integer yearTo,
-        @Schema(example = "1") @NotNull @Min(1) Integer pageNumber,
-        @Schema(example = "5") @NotNull @Min(5) Integer pageSize) {
+        @Schema(example = "1") @Min(1) int pageNumber,
+        @Schema(example = "5") @Min(5) int pageSize) {
 }
